47ccc122ea3276847a3ff7e666da7cb17ca56ea6
efigueredo <emanoel.figueredo.1@gmail.com>
2023-01-15 16:40:31 -0300

    projeto limpo

--- src/test/java/br/com/efigueredo/blackscreen/prototipo_configuracao_resposta/incorreto/ConfiguracaoRespostaPrototipo4.java
+++
@@ -1,9 +0,0 @@
-package br.com.efigueredo.blackscreen.prototipo_configuracao_resposta.incorreto;
-
-import br.com.efigueredo.blackscreen.anotacoes.ConfiguracaoSistema;
-import br.com.efigueredo.blackscreen.sistema.configuracoes.ConfiguracaoSistemaTipo;
-
-@ConfiguracaoSistema(tipo = ConfiguracaoSistemaTipo.RESPOSTAS)
-public class ConfiguracaoRespostaPrototipo4 {
-
-}
